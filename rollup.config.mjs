d86c2575ad8cfdbcb8c8f863ab33ed7672e0e389
Luis Salas <lhozdroid@gmail.com>
2025-02-21 09:14:06 -0500

    Update package.json and rollup.config.mjs to include de-minified build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,12 +1,23 @@
 import terser from "@rollup/plugin-terser";
 
-export default {
-    input: "src/bvalidate.js",
-    output: [
-        {
-            file: "dist/bvalidate.min.js",
-            format: "es"
-        }
-    ],
-    plugins: [terser()]
-};
+export default [
+    {
+        input: "src/bvalidate.js",
+        output: [
+            {
+                file: "dist/bvalidate.min.js",
+                format: "es"
+            }
+        ],
+        plugins: [terser()]
+    },
+    {
+        input: "src/bvalidate.js",
+        output: [
+            {
+                file: "dist/bvalidate.js",
+                format: "es"
+            }
+        ]
+    }
+]
